d1f8b560818359fc842e9c97d15f37f9fae8a626
volbil <volbilnexus@gmail.com>
2023-09-01 12:16:14 +0300

    Fix for get_by_txid

--- backend/services.py
+++ backend/services.py
@@ -12,21 +12,35 @@ from .models import Stats
 from .models import Peer
 from pony import orm
 
+
 class BlockService(object):
     @classmethod
     def latest_block(cls):
-        return Block.select().order_by(
-            orm.desc(Block.height)
-        ).first()
+        return Block.select().order_by(orm.desc(Block.height)).first()
 
     @classmethod
-    def create(cls, blockhash, height, created,
-               merkleroot, version, stake,
-               nonce, size, bits):
+    def create(
+        cls,
+        blockhash,
+        height,
+        created,
+        merkleroot,
+        version,
+        stake,
+        nonce,
+        size,
+        bits,
+    ):
         return Block(
-            blockhash=blockhash, height=height, created=created,
-            merkleroot=merkleroot, version=version, stake=stake,
-            nonce=nonce, size=size, bits=bits
+            blockhash=blockhash,
+            height=height,
+            created=created,
+            merkleroot=merkleroot,
+            version=version,
+            stake=stake,
+            nonce=nonce,
+            size=size,
+            bits=bits,
         )
 
     @classmethod
@@ -42,44 +56,50 @@ class BlockService(object):
 
     @classmethod
     def blocks(cls, page=1, size=100):
-        return Block.select().order_by(
-            orm.desc(Block.height)
-        ).page(page, pagesize=size)
+        return (
+            Block.select()
+            .order_by(orm.desc(Block.height))
+            .page(page, pagesize=size)
+        )
 
     @classmethod
     def time_range(cls, start=None, end=None):
         blocks = Block.select()
 
         if start:
-            blocks = blocks.filter(
-                lambda b: b.created >= start
-            )
+            blocks = blocks.filter(lambda b: b.created >= start)
 
         if end:
-            blocks = blocks.filter(
-                lambda b: b.created < end
-            )
+            blocks = blocks.filter(lambda b: b.created < end)
 
         return blocks
+
 
 class TransactionService(object):
     @classmethod
     def get_by_txid(cls, txid):
-        return Transaction.get(txid=txid)
+        # return Transaction.get(txid=txid)
+        return Transaction.filter(lambda t: t.txid == txid).first()
 
     @classmethod
-    def create(cls, txid, locktime, size, block,
-               coinbase=False, coinstake=False):
+    def create(
+        cls, txid, locktime, size, block, coinbase=False, coinstake=False
+    ):
         return Transaction(
-            txid=txid, locktime=locktime, size=size,
-            coinbase=coinbase, coinstake=coinstake,
-            block=block
+            txid=txid,
+            locktime=locktime,
+            size=size,
+            coinbase=coinbase,
+            coinstake=coinstake,
+            block=block,
         )
 
     @classmethod
     def count(cls, rewards=False):
         # return Transaction.select().count(distinct=False)
-        transactions_key = "total_transactions" if rewards else "non_reward_transactions"
+        transactions_key = (
+            "total_transactions" if rewards else "non_reward_transactions"
+        )
         total = StatsService.get_by_key(transactions_key)
         return int(total.value)
 
@@ -99,13 +119,17 @@ class TransactionService(object):
 
         return query.page(page, pagesize=size)
 
+
 class InputService(object):
     @classmethod
     def create(cls, sequence, n, transaction, vout):
         return Input(
-            sequence=sequence, transaction=transaction,
-            vout=vout, n=n,
+            sequence=sequence,
+            transaction=transaction,
+            vout=vout,
+            n=n,
         )
+
 
 class AddressService(object):
     @classmethod
@@ -113,16 +137,15 @@ class AddressService(object):
         address = Address.get(address=script)
 
         if not address and create:
-            address = AddressService.create(
-                script, lastactive
-            )
+            address = AddressService.create(script, lastactive)
 
         return address
 
     @classmethod
     def richlist(cls, page=1, size=100, currency=CURRENCY):
         query = orm.select(
-            (b.address, b.balance) for b in Balance
+            (b.address, b.balance)
+            for b in Balance
             if b.currency == currency and b.address.address != BURN_ADDRESS
         )
 
@@ -132,21 +155,17 @@ class AddressService(object):
 
     @classmethod
     def create(cls, address, created):
-        return Address(
-            address=address, created=created,
-            lastactive=created
-        )
+        return Address(address=address, created=created, lastactive=created)
 
     @classmethod
     def count(cls):
         return Address.select().count()
 
+
 class BalanceService(object):
     @classmethod
     def get_by_currency(cls, address, currency):
-        balance = Balance.get(
-            address=address, currency=currency
-        )
+        balance = Balance.get(address=address, currency=currency)
 
         if not balance:
             balance = BalanceService.create(address, currency)
@@ -155,9 +174,8 @@ class BalanceService(object):
 
     @classmethod
     def create(cls, address, currency):
-        return Balance(
-            address=address, currency=currency
-        )
+        return Balance(address=address, currency=currency)
+
 
 class OutputService(object):
     @classmethod
@@ -167,12 +185,19 @@ class OutputService(object):
         ).first()
 
     @classmethod
-    def create(cls, transaction, amount, category, address, raw, n,
-               currency=CURRENCY):
+    def create(
+        cls, transaction, amount, category, address, raw, n, currency=CURRENCY
+    ):
         return Output(
-            transaction=transaction, amount=amount, category=category,
-            address=address, raw=raw, n=n, currency=currency
+            transaction=transaction,
+            amount=amount,
+            category=category,
+            address=address,
+            raw=raw,
+            n=n,
+            currency=currency,
         )
+
 
 class StatsService(object):
     @classmethod
@@ -180,11 +205,10 @@ class StatsService(object):
         entry = Stats.get(key=key)
 
         if not entry:
-            entry = Stats(
-                key=key
-            )
+            entry = Stats(key=key)
 
         return entry
+
 
 class PeerService(object):
     @classmethod
@@ -208,16 +232,19 @@ class PeerService(object):
     @classmethod
     def create(cls, address, port, version, subver, active=True):
         return Peer(
-            address=address, port=port, version=version,
-            subver=subver, active=active
+            address=address,
+            port=port,
+            version=version,
+            subver=subver,
+            active=active,
         )
 
     @classmethod
     def location(cls, country, lat, lon, city, code, peer):
         return Location(
-            country=country, lat=lat, lon=lon,
-            city=city, code=code, peer=peer
+            country=country, lat=lat, lon=lon, city=city, code=code, peer=peer
         )
+
 
 class MasternodeService(object):
     @classmethod
@@ -233,15 +260,23 @@ class MasternodeService(object):
 
     @classmethod
     def total(cls):
-        return Masternode.select().filter(
-            lambda m: m.active is True and m.rank is not None
-        ).count()
+        return (
+            Masternode.select()
+            .filter(lambda m: m.active is True and m.rank is not None)
+            .count()
+        )
 
     @classmethod
     def enabled(cls):
-        return Masternode.select().filter(
-            lambda m: m.active is True and m.rank is not None and m.status == "ENABLED"
-        ).count()
+        return (
+            Masternode.select()
+            .filter(
+                lambda m: m.active is True
+                and m.rank is not None
+                and m.status == "ENABLED"
+            )
+            .count()
+        )
 
     @classmethod
     def get_by_address(cls, address):
@@ -249,40 +284,52 @@ class MasternodeService(object):
         return masternode
 
     @classmethod
-    def create(cls, rank, activetime, lastseen, lastpaid,
-               version, address, txhash, outidx, status,
-               pubkey, active=True):
+    def create(
+        cls,
+        rank,
+        activetime,
+        lastseen,
+        lastpaid,
+        version,
+        address,
+        txhash,
+        outidx,
+        status,
+        pubkey,
+        active=True,
+    ):
         return Masternode(
-            rank=rank, activetime=activetime, lastseen=lastseen,
-            lastpaid=lastpaid, version=version, address=address,
-            txhash=txhash, outidx=outidx, status=status,
-            pubkey=pubkey, active=active
+            rank=rank,
+            activetime=activetime,
+            lastseen=lastseen,
+            lastpaid=lastpaid,
+            version=version,
+            address=address,
+            txhash=txhash,
+            outidx=outidx,
+            status=status,
+            pubkey=pubkey,
+            active=active,
         )
+
 
 class IntervalService(object):
     @classmethod
     def get_by_time(cls, key, time):
-        return Interval.get(
-            key=key, time=time
-        )
+        return Interval.get(key=key, time=time)
 
     @classmethod
     def latest(cls, key):
-        return Interval.select(
-            lambda i: i.key == key
-        ).order_by(
-            orm.desc(Interval.time)
-        ).first()
-
-    @classmethod
-    def create(cls, key, time, value=0):
-        return Interval(
-            key=key, time=time,
-            value=value
+        return (
+            Interval.select(lambda i: i.key == key)
+            .order_by(orm.desc(Interval.time))
+            .first()
         )
 
     @classmethod
+    def create(cls, key, time, value=0):
+        return Interval(key=key, time=time, value=value)
+
+    @classmethod
     def list(cls, key):
-        return Interval.select(
-            lambda i: i.key == key
-        ).order_by(Interval.time)
+        return Interval.select(lambda i: i.key == key).order_by(Interval.time)
